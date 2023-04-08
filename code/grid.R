3bd3a612102f568594b87e45988fcef3e729d6b8
temospena <temospena@gmail.com>
2023-04-08 13:59:27 +0100

    bbox grid

--- code/grid.R
+++ code/grid.R
@@ -1,14 +1,34 @@
+# Aim: create a grid with 500m in Lisbon (or in other city)
 
 
-# create a grid with 500m in Lisbon
 library(tidyverse)
 library(sf)
 
 
 
-MUNICIPIOSgeo = readRDS(url("https://github.com/U-Shift/SiteSelection/releases/download/0.1/MUNICIPIOSgeo.Rds")) # not working ?
-MUNICIPIOSgeo = readRDS(url("https://github.com/U-Shift/biclar/releases/download/0.0.1/MUNICIPIOSgeo.Rds"))
+# Get a polygon with the city boundaries ------------------------------------------------------
 
-CITY = "Lisboa"
+MUNICIPIOSgeo = readRDS(url("https://github.com/U-Shift/SiteSelection/releases/download/0.1/MUNICIPIOSgeo.Rds")) # 
+
+CITY = "Lisboa" # change here the city 
 CITYlimit = MUNICIPIOSgeo %>% filter(Concelho == CITY)
 
+plot(CITYlimit)
+
+
+# Make a grid that covers the city polygon ----------------------------------------------------
+
+CITYlimit_meters = st_transform(CITYlimit, 3857) #projected
+cellsize = c(500, 500) #500x500m
+
+GRID = st_make_grid(CITYlimit_meters,
+                    cellsize = cellsize,
+                    offset = st_bbox(CITYlimit_meters)[c("xmin", "ymin")], #bb ist not created correctly! rxplore how to cover properly
+                    # what = "polygons",
+                    # n = c(1, 1),
+                    square = TRUE #FALSE = hexagons
+                    ) 
+plot(CITYlimit_meters)
+plot(GRID, add = TRUE)
+
+#bbox ist not created correctly! rxplore how to cover properly
