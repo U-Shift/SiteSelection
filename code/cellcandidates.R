87e3a5b63ae7bb1efabb0f545e07be3a93b8fe93
temospena <temospena@gmail.com>
2023-05-17 01:06:13 +0100

    adjust cell candidates
    needs review @valenca13

--- code/cellcandidates.R
+++ code/cellcandidates.R
@@ -1,15 +1,21 @@
 # aim: select the appropriate cells depending on their indicators
 
+
+
+# Centrality ----------------------------------------------------------------------------------
+
 CELL_candidates = left_join(GRID, centrality_grid)
 
-# CENTRALITY
+plot(CELL_candidates["betweenness"]) 
+plot(CELL_candidates["closeness"]) 
+plot(CELL_candidates["degree"]) 
 
+# Filter in thresholds
 CELL_candidates = CELL_candidates %>% 
   filter(betweenness %in% (0.25:0.75), #too high
          closeness %in% (0.25:0.75),  #too high
          degree >= 0.5) #for Lisbon. Adjust for other places?
 
-plot(CELL_candidates["geometry"]) # zero results
          
 # 
 # LANDUSE
