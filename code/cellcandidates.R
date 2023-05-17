9169ed54cf8449d31c91a3c2e580ed250a92e553
RFlx <temospena@gmail.com>
2023-05-17 17:57:08 +0100

    filter condition above and below

--- code/cellcandidates.R
+++ code/cellcandidates.R
@@ -6,21 +6,28 @@
 
 CELL_candidates = left_join(GRID, centrality_grid)
 
+plot(CELL_candidates["degree"]) 
 plot(CELL_candidates["betweenness"]) 
 plot(CELL_candidates["closeness"]) 
-plot(CELL_candidates["degree"]) 
+
 
 # Filter in thresholds
 CELL_candidates = CELL_candidates %>% 
-  filter(betweenness %in% (0.25:0.75), #too high
-         closeness %in% (0.25:0.75),  #too high
-         degree >= 0.5) #for Lisbon. Adjust for other places?
-
+  filter(degree >= 0.5, #1088 #for Lisbon. Adjust for other places?
+         betweenness >= 0.25 & betweenness <= 0.75, #207 #too high
+         closeness >= 0.25 & closeness <= 0.75 #2135 #too high
+         ) 
+# 56 results
+mapview::mapview(CELL_candidates)
          
-# 
-# LANDUSE
+
+# Land use ----------------------------------------------------------------
+
 # density -> higher than mean(density)
 # entropy -> hiher than 0.5 (in 4 land uses, have at least 2)
 
-# TIME
-# traffic -> ?
+
+
+# Traffic -----------------------------------------------------------------
+
+# traffic -> high (max(traffic) in each cell is 3 or 4), (1 and 2 is low)
