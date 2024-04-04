2ddbb6e3f1c7dbfcb143a1eccb2358922a93ed41
Gabriel Valença <69808511+valenca13@users.noreply.github.com>
2024-04-04 17:19:46 +0100

    Update transit.R

--- code/transit.R
+++ code/transit.R
@@ -12,8 +12,8 @@
     
     #Operador: Transporlis
     
-    Braga_gtfs_2_zip = "https://gtfs.pro/files/uran/improved-gtfs-braga.zip" # 1.6MB 
-    download.file(Braga_gtfs_2_zip, destfile = "database/transit/braga_gtfs_transporlis.zip")
+   # Braga_gtfs_2_zip = "https://gtfs.pro/files/uran/improved-gtfs-braga.zip" # 1.6MB 
+   # download.file(Braga_gtfs_2_zip, destfile = "database/transit/braga_gtfs_transporlis.zip")
     
 
   # Lisbon
@@ -76,7 +76,7 @@
 # read gtfs files
     
     braga_4Planning_gtfs <- read_gtfs("database/transit/braga_gtfs_4planning.zip")
-    braga_transporlis_gtfs <- read_gtfs("database/transit/braga_gtfs_transporlis.zip")
+  #  braga_transporlis_gtfs <- read_gtfs("database/transit/braga_gtfs_transporlis.zip")
     lisbon_gtfs <- read_gtfs("database/transit/lisbon_gtfs.zip")
     aml_gtfs <- read_gtfs("database/transit/AML_gtfs.zip")
     funchal_gtfs <- read_gtfs("database/transit/funchal_gtfs.zip")
@@ -89,9 +89,13 @@
     
     # Select and filter databases by a representative date (Wednesday)
 
+    #MODIFICAR DATAS
+    
     braga_4Planning_date <- filter_feed_by_date(braga_4Planning_gtfs,"2024-04-03")
-    braga_transporlis_date <- filter_feed_by_date(braga_transporlis_gtfs, "2024-03-10")
-    lisbon_date <- filter_feed_by_date(lisbon_gtfs, "2024-03-10")
+ #   braga_transporlis_date <- filter_feed_by_date(braga_transporlis_gtfs, "2024-03-10")
+    lisbon_date <- filter_feed_by_date(lisbon_gtfs, "2024-04-10")
+    
+    
     aml_date <- filter_feed_by_date(aml_gtfs, "2024-03-10")
     funchal_date <- filter_feed_by_date(funchal_gtfs, "2024-03-10")
     cascais_date <- filter_feed_by_date(cascais_gtfs, "2024-03-10")
@@ -101,7 +105,7 @@
     
     #Organize the table calculating the frequencies per bus stop
     
-    # Braga 4-Planning
+#### Braga 4-Planning
     
       #Get stop frequency (missing data)
     
@@ -150,68 +154,69 @@
     mapview::mapview(braga_4Planning_table) 
     
     
+#### Lisbon
+    
+    #Get stop frequency (missing data)
+    
+    lisbon_f = data.frame()
+    
+    for (i in 6:9){
+      lisbon <- get_stop_frequency(lisbon_date,
+                                            start_time = paste0("0",i,":00:00"),
+                                            end_time = paste0("0",i,":59:59"),
+                                            service_ids = NULL,
+                                            by_route = TRUE) 
+      
+      lisbon <- lisbon |> 
+        group_by(stop_id) |> 
+        summarise(frequency = sum(n_departures)) |> 
+        mutate(hour=i)
+      lisbon_f = rbind(lisbon_f, lisbon)
+    }
+    
+    for (i in 10:23) {
+      lisbon <- get_stop_frequency(lisbon_date,
+                                              start_time = paste0(i,":00:00"),
+                                              end_time = paste0(i,":59:59"),
+                                              service_ids = NULL,
+                                              by_route = TRUE)
+      
+      lisbon <- lisbon |> 
+        group_by(stop_id) |> 
+        summarise(frequency = sum(n_departures)) |> 
+        mutate(hour=i)
+      lisbon_f =rbind(lisbon_f, lisbon)
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-    excluir = c("stop_code", "stop_desc", "zone_id", "stop_url", "location_type", "parent_station", "stop_headsign", "pickup_type",
-                "drop_off_type", "shape_dist_traveled", "trip_headsign", "direction_id", "block_id", "route_short_name",
-                "route_desc","route_url", "route_color", "route_text_color")
-    
-    braga_4Planning_table = braga_4Planning_table |> 
-      select(!all_of(excluir))
-    
-    
-    braga_4Planning_redux = braga_4Planning_table |>
-      group_by(stop_id, stop_name, stop_lat, stop_lon) |> 
-      summarise(frequency = n()) |> 
+    lisbon_frequency <- lisbon_f |> 
+      ungroup() |> 
+      group_by(stop_id,hour) |> 
+      summarise(frequency = sum(frequency)) |> 
       ungroup()
-    sum(braga_4Planning_redux$frequency) #all: 376697, only 16jan: 135557
-    
-    braga_4Planning_stops = st_as_sf(Carris_stops_redux, coords = c("stop_lon", "stop_lat"), crs=4326)
-    
-    st_write(Carris_stops, "database/transit/shapefiles/braga_4Planning_stops.gpkg", delete_dsn = TRUE)
-    
-    #
     
     
+    lisbon_table <- lisbon_frequency |> 
+      left_join(lisbon_date$stops |> 
+                  select(stop_id,stop_lon,stop_lat), by = "stop_id") |> 
+      st_as_sf(crs=4326, coords = c("stop_lon","stop_lat"))
+    
+    mapview::mapview(lisbon_table)     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-
-gtfs de todos as operadoras a nível nacional
-filtrar as que são underground
-validar
-verificar data comum /(senão traspor)
-gtfs merge
-
-
-# calcular frequencia por hora por paragem --------------------------------
-
-ficar com o valor máximo horário num dia típico
-
-
-# procurar paragens no poligono -------------------------------------------
-
-soma da média de frequencia de todas as paragens dentro do poligono
-
-
-# classificacao -----------------------------------------------------------
-
-ver o ranking do artigo (NACTO) e classificar a grid
-
-
+    
+   
