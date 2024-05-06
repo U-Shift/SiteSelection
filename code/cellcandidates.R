4c47fe98b83a1318aece78c9cd7a2302c01b9971
Gabriel Valença <69808511+valenca13@users.noreply.github.com>
2024-05-06 11:13:04 +0100

    Update cellcandidates.R

--- code/cellcandidates.R
+++ code/cellcandidates.R
@@ -28,7 +28,44 @@ mapview::mapview(CELL_candidates)
 # density -> higher than mean(density)
 # entropy -> hiher than 0.5 (in 4 land uses, have at least 2)
 
+# Public transport
 
+library(sf)
+library(tidyverse)
+gtfs <- st_read("database/bus_stop_frequency_gtfs.gpkg")
+
+# Join bus stops in grid 
+
+gtfs_1 = st_join(gtfs, GRID, join = st_intersects)
+
+# Calculate the frequency sum per hour within a zone
+
+gtfs_2 = gtfs_1 |> 
+  group_by(ID, hour) |> 
+  summarise(frequency = sum(frequency))
+
+# Calculate the maximum frequency of stops_id per day
+
+gtfs_3 = gtfs_2 |>  
+  group_by(ID) |>  
+  summarise(max_frequency = max(frequency))
+
+# Calculate the level of service of each stop_id
+#if max_frequency <= 4 -> 1
+#if max_frequency > 4 and <= 10 -> 2
+#if max_frequency > 10 and <= 20 -> 3
+#if max_frequency > 20 -> 4
+
+gtfs_4 = gtfs_3 |> 
+  mutate(level = case_when(
+    max_frequency <= 4 ~ 1,
+    max_frequency > 4 & max_frequency <= 10 ~ 2,
+    max_frequency > 10 & max_frequency <= 20 ~ 3,
+    max_frequency > 20 ~ 4
+  ))
+
+# st_write(gtfs_4, "bus_stop_LS_gtfs_final.gpkg")
+ 
 
 # Traffic -----------------------------------------------------------------
 
