6edd01fc02d64d33896a7427be4c11d85af551a1
RFlx <temospena@gmail.com>
2024-05-23 20:43:53 +0100

    advances in traffic
    #12

--- code/traffic_vector.R
+++ code/traffic_vector.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(sf)
 library(mapboxapi)
 
+Sys.getenv("MAPBOX_PUBLIC_TOKEN") # exists!
+get_mb_access_token()
 
 # Get data in vector format
 # CITY = "Lisboa"
@@ -14,9 +16,11 @@ library(mapboxapi)
 # saveRDS(CITYlimit, "database/lisbon_limit.Rds")
 CITYlimit = readRDS("database/lisbon_limit.Rds")
 
+CELL_candidates = site_selection
+
 traffic_vector = get_vector_tiles(
   tileset_id = "mapbox.mapbox-traffic-v1",
-  location = CITYlimit,
+  location = CELL_candidates,
   zoom = 14 #14
   )
 traffic_vector = traffic_vector$traffic
@@ -35,4 +39,75 @@ st_write(traffic_vector,
             delete_dsn = TRUE)
 
 
+# with a loop -------------------------------------------------------------
 
+
+CELL_candidates = site_selection
+traffic_vector_total = traffic_vector_i[1,] # pre-run with 1 case
+
+for (i in c(1:nrow(CELL_candidates))){
+  traffic_vector = get_vector_tiles(
+    tileset_id = "mapbox.mapbox-traffic-v1",
+    location = CELL_candidates[i,],
+    zoom = 17
+  )
+  
+  traffic_vector_i = traffic_vector$traffic
+  
+    if("structure" %in% colnames(traffic_vector_i) == TRUE){
+      traffic_vector_i = traffic_vector_i |> filter(!structure %in% c("tunnel", "bridge"))
+    }else{
+      traffic_vector_i = traffic_vector_i |> mutate(structure = NA)
+          }
+  
+    traffic_vector_i = traffic_vector_i |>
+      select(congestion, geometry)
+  traffic_vector_total = bind_rows(traffic_vector_total, traffic_vector_i) |>
+    unique()
+  
+}
+
+traffic_vector_total = traffic_vector_total[-1,]
+traffic_vector_total$congestion = factor(traffic_vector_total$congestion,
+                                      levels = c("low", "moderate", "heavy", "severe"),
+                                      ordered = TRUE)
+  
+  #Visualise
+  mapview::mapview(traffic_vector_total, zcol="congestion") + mapview::mapview(CELL_candidates)
+  
+  
+  # crop
+  traffic_vector_total_cropped = st_intersection(traffic_vector_total, CELL_candidates |> select(ID))
+  
+  # How to classify if a cell presents congestion?
+  traffic_vector_total_cropped = traffic_vector_total_cropped |> 
+    mutate(length = st_length(geometry) |> units::drop_units())
+
+
+  traffic_vector_total_cropped_cell_length = traffic_vector_total_cropped |> 
+    st_drop_geometry() |>
+    group_by(ID) |> 
+    summarise(length = sum(length)) |> 
+    ungroup()
+  
+  
+  # classify if a cell presents congestion (heavy or severe) in more than 10% of total length
+  traffic_vector_total_cropped_classified = traffic_vector_total_cropped |>
+    st_drop_geometry() |>
+    filter(congestion %in% c("heavy", "severe")) |>
+    group_by(ID) |>
+    summarise(length = sum(length)) |>
+    left_join(traffic_vector_total_cropped_cell_length, by = "ID") |>
+    mutate(classification = ifelse(length.x/length.y > 0.1, "congested", "free")) # 10% ?
+  
+  # get only the cells that are congested
+  traffic_vector_total_cropped_congested = traffic_vector_total_cropped_classified |>
+    select(ID, classification)
+  
+  CELL_candidates = CELL_candidates |>
+    left_join(traffic_vector_total_cropped_congested, by = "ID")
+    
+  # vizualise
+  mapview::mapview(CELL_candidates, zcol="classification", alpha.regions = 0.2) +
+    mapview::mapview(traffic_vector_total, zcol="congestion")
+ 
