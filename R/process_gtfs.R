0f21d2d8cae88e7791bc722ace94901e69bf9789
goncaloafmatos <goncaloafmatos@tecnico.ulisboa.pt>
2025-02-05 17:12:57 +0000

    #25 proces_gtfs improved to support multiple areas and generate gpkg with aggregated output

--- R/process_gtfs.R
+++ R/process_gtfs.R
@@ -10,21 +10,31 @@ library(tidytransit)
 #' @param gtfs_url The url of the GTFS zip file
 #' @param city String with zity name
 #' @param date Reference date to consider when analysing the GTFS file
-process_gtfs <- function(gtfs_url, city, date) {
-  print(sprintf("\nAnalysing GTFS for %s...", city))
+process_gtfs <- function(gtfs_url, area, date) {
+  print(sprintf("Analysing GTFS for %s...", area))
 
   # Download GTFS and store it locally
   if (!dir.exists("database/transit")) {
     dir.create("database/transit", recursive = TRUE)
   }
   print(sprintf("> Downloading GTFS file..."))
-  destfile <- sprintf("database/transit/%s_gtfs.zip", city)
+  destfile <- sprintf("database/transit/%s_gtfs.zip", area)
   download.file(gtfs_url, destfile = destfile)
+  print(sprintf("> GTFS downloaded and stored at %s!", destfile))
 
   # Open GTFS with tidytransit library and filter by date
-  print(sprintf("> Analysing reference date %s...", date))
+  print("> Openning it for processing...")
   gtfs <- read_gtfs(destfile)
+  print(sprintf("> Openned GTFS for %s (ID %s)!", gtfs$agency$agency_name, gtfs$agency$agency_id))
   
+  # Fix GTFS 
+  stopsNPrev <- length(gtfs$stop_times$trip_id)
+  gtfs$stop_times <- gtfs$stop_times[!is.na(gtfs$stop_times$arrival_time), ] # Cascais GTFS has stop_times with empty arrival/departure times, which raises an error at filter_feed_by_date method
+  stopsNAfter <- length(gtfs$stop_times$trip_id)
+  
+  if (stopsNPrev != stopsNAfter) {print(sprintf("> FIXED GTFS, there were %d stop times without arrival time!", stopsNPrev-stopsNAfter))}
+  
+  print(sprintf("> Analysing reference date %s...", date))
   gtfs_date <- filter_feed_by_date(
     gtfs, extract_date = date
   )
@@ -40,10 +50,11 @@ process_gtfs <- function(gtfs_url, city, date) {
 
   ### Building meta data on the service patterns 
   ### https://cran.r-project.org/web/packages/tidytransit/vignettes/servicepatterns.html
+  ### Alternative docs: https://cran.r-project.org/web/packages/tidytransit/tidytransit.pdf, page 20
   ### Creates $.$servicepatterns with unique id per pattern
   ### Creates $.$dates_servicepatterns matching each individual date covered by the GTFS with the corresponding id
   pattern_gtfs <- set_servicepattern(gtfs_date)
-  print(sprintf("> Identified %d service patterns matching date: %s", length(pattern_gtfs$.$servicepatterns$servicepattern_id), pattern_gtfs$.$servicepatterns$service_id))
+  print(sprintf("> Identified %d service patterns matching date: %s", length(pattern_gtfs$.$servicepatterns$servicepattern_id), paste(pattern_gtfs$.$servicepatterns$service_id, collapse=", ")))
   ### WARNING: every time we run this, random ids will be generated for the service patterns
 
   ## Convert stops and shapes to simple features
@@ -116,13 +127,43 @@ process_gtfs <- function(gtfs_url, city, date) {
     left_join(gtfs_date$stops |>
     select(stop_id, stop_lon, stop_lat), by = "stop_id") |>
     st_as_sf(crs = 4326, coords = c("stop_lon", "stop_lat"))
+  
+  ## Prepend stop_id with GTFS.agency.agency_id to avoid duplicate stop ids from multiple GTFS merging
+  table$stop_id <- paste0(sprintf("%s_", gtfs$agency$agency_id), table$stop_id)
 
+  print("Finished GTFS analysis!")
+  
   return(table)
 }
 
 # main()
-barreiro_gtfs = process_gtfs(
-  "https://www.tcbarreiro.pt/front/files/sample_gtfs/GTFS-TCB_24.zip",
-  "barreiro",
-  "2025-02-05"
+
+request <- tribble(
+  ~area, ~date, ~url,
+  "barreiro", "2025-02-05", "https://www.tcbarreiro.pt/front/files/sample_gtfs/GTFS-TCB_24.zip", # VALIDATED against CityMapper
+  "braga", "2025-02-05", "https://tub.pt/developer/gtfs/feed/tub.zip", # VALIDATED against previous version
+  "lisboa", "2025-02-05", "https://gateway.carris.pt/gateway/gtfs/api/v2.8/GTFS", # VALIDATED against CityMapper
+  "AML", "2025-02-05", "https://api.carrismetropolitana.pt/gtfs", # VALIDATED against CityMapper
+  "cascais", "2025-02-05", "https://drive.google.com/u/0/uc?id=13ucYiAJRtu-gXsLa02qKJrGOgDjbnUWX&export=download", # VALIDATED against previous version
+  "porto", "2025-02-05", "https://opendata.porto.digital/dataset/5275c986-592c-43f5-8f87-aabbd4e4f3a4/resource/1e0f4315-3694-42b0-a8ce-5218ad4742e5/download/horarios_gtfs_stcp_06_01_2025.zip" # VALIDATED against previous version
 )
+output_file <- "database/transit/bus_stop_frequency.gpkg"
+
+aggregated_frequencies <- data.frame()
+
+for (area in request$area) {
+  frequencies <- process_gtfs(
+    request$url[request$area == area],
+    area,
+    request$date[request$area == area]
+  )
+  
+  assign(sprintf("frequencies_%s", area), frequencies)
+  
+  aggregated_frequencies <- rbind(aggregated_frequencies, frequencies)
+}
+
+print(sprintf("Finished processing! Storing output to %s...", output_file))
+st_write(aggregated_frequencies, output_file, append=FALSE) # append=FALSE for overwrite
+
+print("Done! :)")
